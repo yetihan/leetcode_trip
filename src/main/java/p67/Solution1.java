b447b3cd7bd2865005ece6bd1576a4617ef3e152
yetihan <h_hzhou@sina.cn>
2019-04-11 22:18:32 +0800

    67,自己写一个蠢解法,抄一个网上的

---
+++ src/main/java/p67/Solution1.java
@@ -0,0 +1,42 @@
+package p67;
+
+/**
+ * https://leetcode.com/problems/add-binary/
+ *
+ * Given two binary strings, return their sum (also a binary string).
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ *
+ * 难度 easy
+ * 2019年04月11日21:40:37
+ *
+ * 20% 56%
+ */
+
+public class Solution1 {
+    public static String addBinary(String a, String b) {
+        StringBuilder sum = new StringBuilder();
+        int maxLen = Math.max(a.length(), b.length());
+        int extra = 0; // 标记是否进位
+        int a_bit, b_bit, tmp;
+        for (int i = 0; i < maxLen; i++) {
+            a_bit = i < a.length() ? Integer.parseInt(a.substring (a.length() -1 - i,a.length()-i)) : 0;
+            b_bit = i < b.length() ? Integer.parseInt(b.substring (b.length() -1 - i,b.length()-i)) : 0;
+            tmp = a_bit+b_bit+extra;
+            extra = 0;
+            if(tmp<=1) sum.append(tmp);
+            else if(tmp==2){
+                sum.append(0);
+                extra=1;
+            }else {
+                sum.append(1);
+                extra=1;
+            }
+        }
+        if(extra==1) sum.append(1);
+        return sum.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addBinary("1010", "1011"));
+    }
+}
---
+++ src/main/java/p67/Solution2.java
@@ -0,0 +1,47 @@
+package p67;
+
+/**
+ * https://leetcode.com/problems/add-binary/
+ *
+ * Given two binary strings, return their sum (also a binary string).
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ *
+ * 难度 easy
+ * 2019年04月11日21:40:37
+ *
+ * char相减
+ *
+ * 99% 41%
+ */
+
+public class Solution2 {
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int m = a.length() -1;
+        int n = b.length() -1 ;
+        int extra = 0; // 标记是否进位
+        while (m>=0 || n>=0){
+            int sum = extra;
+            if(m>=0){
+                sum += (a.charAt(m)-'0');
+                m--;
+
+            }
+
+            if(n>=0){
+                sum += (b.charAt(n)-'0');
+                n--;
+            }
+            // 这一步 比我写的简单多了,^_^
+            sb.append(sum%2);
+            extra = sum/2;
+        }
+
+        if(extra==1) sb.append(1);
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addBinary("1", "111"));
+    }
+}
