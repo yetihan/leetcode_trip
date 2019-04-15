3ca9a4810ad7f4a6a44b27cbc64906c03dbf5f1c
韩洲(hanzhou) <hanzhou@zenmen.com>
2019-04-15 14:23:07 +0800

    双指针法复杂度为O(n),利用简单的数学证明

---
+++ src/main/java/p11/Solution3.java
@@ -0,0 +1,36 @@
+package p11;
+
+/**
+ * https://leetcode.com/problems/container-with-most-water/
+ * medium
+ * <p>
+ * 双指针 tow-pointer
+ * 一个从最左边开始,一个从最右边开始,都逐渐往中间移动,
+ * 面积取决于宽度和高度,移动过程中,宽度总是在减小,故我们总是选择移动目前比较矮的那个;
+ * 因为,目前的面积取决于矮边的高度,那么固定矮边,长边所有的其他情况与现在相比,
+ * 高度一定只会<=目前的高度,长度<目前的长度,所以面积只会减小. 证毕!!!
+ * <p>
+ * Runtime: 397 ms, faster than 5.01% of Java online submissions for Container With Most Water.
+ */
+
+public class Solution3 {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = (height.length - 1) * Math.max(height[left], height[right]);
+        while (left < right) {
+            if (height[left] < height[right]) {
+                left++;
+            } else right--;
+
+            int area = (right - left) * Math.max(height[left], height[right]);
+            if (area > maxArea) maxArea = area;
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
